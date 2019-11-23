fe06b94e65b2527e89a9804011d1da82c5079609
Michael Hughes <mikee.hughes@gmail.com>
2019-11-23 22:25:46 +0000

    Format code

--- m6502_lib/CPU.cpp
+++ m6502_lib/CPU.cpp
@@ -7,7 +7,9 @@
 
 #ifndef CLASS_OPCODE
 #define CLASS_OPCODE
+
 #include "OpCode.cpp"
+
 #endif
 
 #include "OpLog.cpp"
@@ -30,7 +32,7 @@ class CPU {
     bool decimalFlag = false;
     bool overflowFlag = false;
     bool negativeFlag = false;
-    std::function<void ()> cycleCallback;
+    std::function<void()> cycleCallback;
     std::array<uint8_t, SIZE> memory;
     uint16_t breakLocation = 0;
     bool doBreak = false;
@@ -56,7 +58,7 @@ class CPU {
 
     uint16_t read16From(uint16_t location) {
         uint8_t upper = memory[location];
-        uint8_t lower = memory[location+1];
+        uint8_t lower = memory[location + 1];
         return toUInt16(upper, lower);
     }
 
@@ -131,37 +133,48 @@ class CPU {
     bool ACC() {
         return true;
     }
+
     uint8_t I() {
         return readUInt8();
     }
+
     uint16_t Z() {
         return readUInt8();
     }
+
     uint16_t ZX() {
         return (Z() + XRegister) % 0x100;
     }
+
     uint16_t ZY() {
         return (Z() + YRegister) % 0x100;
     }
+
     uint16_t Ab() {
         return readUInt16();
     }
+
     uint16_t AbX() {
         return Ab() + XRegister;
     }
+
     uint16_t AbY() {
         return Ab() + YRegister;
     }
+
     bool IMP() {
         return false;
     }
+
     uint16_t INDIR() {
         return read16From(Ab());
     }
+
     uint16_t IndexIndir() {
         auto zp = (Z() + XRegister) % 0x100;
         return read16From(zp);
     }
+
     uint16_t IndirIndex() {
         auto zp = readUInt8();
         auto loc = read16From(zp);
@@ -176,44 +189,49 @@ class CPU {
         }
         overflowFlag = false;
 #ifndef NDEBUG
-        std::cout << "ADC reg:" << std::hex << (int)ARegister << " val:" << (int)value << " sum:" << (int)sum << std::endl;
+        std::cout << "ADC reg:" << std::hex << (int) ARegister << " val:" << (int) value << " sum:" << (int) sum
+                  << std::endl;
 #endif
         ARegister = sum;
         zeroFlag = ARegister == 0;
         negativeFlag = ARegister >> 7 != 0;
     }
+
     void opAddWithCarry(uint16_t location) {
         opAddWithCarry(memory[location]);
     }
 
     void opArithmeticShiftLeft(bool _) {
         auto setTo = ARegister << 1u;
-        setCarryFlag(setTo>0xff);
+        setCarryFlag(setTo > 0xff);
         setARegister(setTo);
     }
+
     void opArithmeticShiftLeft(uint16_t location) {
         auto mem = memory[location];
         auto setTo = mem << 1u;
-        setCarryFlag(setTo>0xff);
+        setCarryFlag(setTo > 0xff);
         setFlagsBasedOnValue(setTo);
-        memory[location]=setTo;
+        memory[location] = setTo;
     }
 
     void opAnd(uint8_t value) {
         setARegister(ARegister & value);
     }
+
     void opAnd(uint16_t location) {
         opAnd(memory[location]);
     }
+
     void opBit(uint16_t location) {
         auto mem = memory[location];
 #ifndef NDEBUG
-        std::cout << std::hex << "BIT A:" << (int)ARegister << " ZP:" << (int)mem << std::endl;
+        std::cout << std::hex << "BIT A:" << (int) ARegister << " ZP:" << (int) mem << std::endl;
 #endif
         auto bit = ARegister & mem;
-        setZeroFlag(bit ==0);
+        setZeroFlag(bit == 0);
         setNegativeFlag((mem & 0x80) << 7);
-        setOverflowFlag( (mem & 0x40) << 6);
+        setOverflowFlag((mem & 0x40) << 6);
     }
 
     void opBranchonCarryClear(bool _) {
@@ -223,6 +241,7 @@ class CPU {
     void opBranchonCarrySet(bool _) {
         branchIfTrue(carryFlag);
     }
+
     void opBranchOnOverflowClear(bool _) {
         branchIfTrue(!overflowFlag);
     }
@@ -238,12 +257,15 @@ class CPU {
     void opBranchOnNotEqual(bool _) {
         branchIfTrue(!zeroFlag);
     }
-    void opBranchOnPlus(bool _){
+
+    void opBranchOnPlus(bool _) {
         branchIfTrue(!negativeFlag);
     }
+
     void opBranchOnMinus(bool _) {
         branchIfTrue(negativeFlag);
     }
+
     void opBreak(bool _) {
         if (breakLocation > 0x00) {
             pushStack16(programCounter + 1);
@@ -251,213 +273,268 @@ class CPU {
             pushStack8(flagsAsInt());
             programCounter = read16From(breakLocation);
         }
-        doBreak=true;
+        doBreak = true;
     }
+
     void opClearCarry(uint16_t _) {
-        carryFlag=false;
+        carryFlag = false;
     }
+
     void opClearDecimal(uint16_t _) {
-        decimalFlag=false;
+        decimalFlag = false;
     }
+
     void opClearInterrupt(uint16_t _) {
-        interruptDisableFlag=false;
+        interruptDisableFlag = false;
     }
+
     void opClearOverflow(uint16_t _) {
-        overflowFlag=false;
+        overflowFlag = false;
     }
+
     void opCompareAcc(uint8_t value) {
         compareRegisterTo(ARegister, value);
     }
+
     void opCompareAcc(uint16_t location) {
         compareRegisterTo(ARegister, memory[location]);
     }
+
     void opCompareX(uint16_t location) {
         compareRegisterTo(XRegister, memory[location]);
     }
+
     void opCompareX(uint8_t value) {
         compareRegisterTo(XRegister, value);
     }
+
     void opCompareY(uint8_t value) {
         compareRegisterTo(YRegister, value);
     }
+
     void opCompareY(uint16_t location) {
         compareRegisterTo(YRegister, memory[location]);
     }
+
     void opDecrement(uint16_t location) {
         auto val = memory[location];
-        val=val-1;
+        val = val - 1;
         setFlagsBasedOnValue(val);
-        memory[location]=val;
+        memory[location] = val;
     }
+
     void opDecrementX(bool _) {
-        setXRegister(XRegister-1);
+        setXRegister(XRegister - 1);
     }
+
     void opDecrementY(bool _) {
-        setYRegister(YRegister-1);
+        setYRegister(YRegister - 1);
     }
+
     void opExclusiveOR(uint8_t value) {
         setARegister(ARegister ^ value);
     }
+
     void opExclusiveOR(uint16_t location) {
         opExclusiveOR(memory[location]);
     }
+
     void opIncrement(uint16_t location) {
         auto val = memory[location];
-        val=val+1;
+        val = val + 1;
         setFlagsBasedOnValue(val);
-        memory[location]=val;
+        memory[location] = val;
     }
+
     void opIncrementX(bool _) {
         setXRegister(XRegister + 1);
     }
+
     void opIncrementY(bool _) {
         setYRegister(YRegister + 1);
     }
+
     void opJump(uint16_t location) {
         programCounter = location;
     }
+
     void opJumpToSubroutine(uint16_t location) {
         pushStack16(programCounter);
         programCounter = location;
     }
+
     void opORwithAcc(uint8_t value) {
         setARegister(ARegister | value);
     }
+
     void opORwithAcc(uint16_t location) {
         opORwithAcc(memory[location]);
     }
+
     void opLoadAcc(uint16_t location) {
         setARegister(memory[location]);
     }
+
     void opLoadAcc(uint8_t value) {
         setARegister(value);
     }
+
     void opLoadX(uint8_t value) {
         setXRegister(value);
     }
+
     void opLoadX(uint16_t location) {
         setXRegister(memory[location]);
     }
+
     void opLoadY(uint8_t value) {
         setYRegister(value);
     }
+
     void opLoadY(uint16_t location) {
         setYRegister(memory[location]);
     }
+
     void opNoop(bool _) {
 
     }
+
     void opLogicalShiftRight(bool _) {
         auto setTo = ARegister >> 1u;
         setCarryFlag(ARegister & 1u);
         setARegister(setTo);
     }
+
     void opLogicalShiftRight(uint16_t location) {
         auto mem = memory[location];
         auto setTo = mem >> 1u;
         setCarryFlag(mem & 1u);
         setFlagsBasedOnValue(setTo);
-        memory[location]=setTo;
+        memory[location] = setTo;
     }
+
     void opPushProcessorStatus(bool _) {
         pushStack8(flagsAsInt());
     }
+
     void opPullProcessorStatus(bool _) {
         intToFlags(popStack8());
     }
+
     void opPushAcc(bool _) {
         pushStack8(ARegister);
     }
+
     void opPullAcc(bool _) {
         setARegister(popStack8());
     }
+
     void opReturnfromInterrupt(uint16_t _) {
         intToFlags(popStack8());
         uint16_t counter = popStack16();
         programCounter = counter;
     }
+
     void opReturnfromSubroutine(uint16_t _) {
         uint16_t counter = popStack16();
         programCounter = counter;
     }
+
     void opRotateLeft(bool _) {
         auto setTo = ARegister << 1u | carryFlag;
         setCarryFlag(ARegister & 0x80u);
         setARegister(setTo);
     }
+
     void opRotateLeft(uint16_t location) {
         auto mem = memory[location];
         auto setTo = mem << 1u | carryFlag;
         setCarryFlag(mem & 0x80u);
         setFlagsBasedOnValue(setTo);
-        memory[location]=setTo;
+        memory[location] = setTo;
     }
+
     void opRotateRight(bool _) {
         auto setTo = ARegister >> 1u | (carryFlag << 7u);
         setCarryFlag(ARegister & 0x1u);
         setARegister(setTo);
     }
+
     void opRotateRight(uint16_t location) {
         auto mem = memory[location];
         auto setTo = mem >> 1u | (carryFlag << 7u);
         setCarryFlag(mem & 0x1u);
         setFlagsBasedOnValue(setTo);
-        memory[location]=setTo;
+        memory[location] = setTo;
     }
+
     void opSetCarry(bool _) {
-        carryFlag=true;
+        carryFlag = true;
     }
+
     void opSetDecimal(bool _) {
-        decimalFlag=true;
+        decimalFlag = true;
     }
+
     void opSetInterrupt(bool _) {
-        interruptDisableFlag=true;
+        interruptDisableFlag = true;
     }
+
     void opSubtractWithCarry(uint16_t location) {
-        auto mem=memory[location];
+        auto mem = memory[location];
         uint8_t sum = ARegister - mem - !isCarryFlag();
         setFlagsBasedOnValue(sum);
-        if(mem > ARegister) {
+        if (mem > ARegister) {
             setNegativeFlag(true);
         }
         setOverflowFlag(false);
-        std::cout << "SBC reg:" << std::hex << (int)ARegister << " val:" << (int)mem << " res:" << (int)sum << std::endl;
+        std::cout << "SBC reg:" << std::hex << (int) ARegister << " val:" << (int) mem << " res:" << (int) sum
+                  << std::endl;
         ARegister = sum;
     }
+
     void opStoreAcc(uint16_t location) {
         memory[location] = ARegister;
     }
+
     void opStoreX(uint16_t location) {
         memory[location] = XRegister;
     }
+
     void opStoreY(uint16_t location) {
         memory[location] = YRegister;
     }
+
     void opTransferAtoX(bool _) {
         setXRegister(ARegister);
     }
+
     void opTransferAtoY(bool _) {
         setYRegister(ARegister);
     }
+
     void opTransferStacktoX(bool _) {
         setXRegister(stackPointer);
     }
+
     void opTransferXtoA(bool _) {
         setARegister(XRegister);
     }
+
     void opTransferXtoStack(bool _) {
         stackPointer = XRegister;
     }
+
     void opTransferYtoA(bool _) {
         setARegister(YRegister);
     }
 
 public:
-    CPU(uint16_t programCounter, std::array<uint8_t, SIZE> memory, std::function<void ()> cycle) {
-        this->cycleCallback = cycle;
+    CPU(uint16_t programCounter, std::array<uint8_t, SIZE> mem, std::function<void()> cycle)
+            : memory(mem),
+              cycleCallback(cycle) {
         this->programCounter = programCounter;
         this->previousProgramCounter = programCounter;
-        this->memory = memory;
     }
 
     void run() {
@@ -467,7 +544,9 @@ public:
             auto opLog = OpLog(opCode, previousProgramCounter);
             switch (opCode) {
 #define OPCODE(name, code, function, mode) case name : function(mode());break;
+
 #include "OpCodeMacro.cpp"
+
                 default:
                     std::cout << "Unknown OpCode:" << std::endl;
 #ifndef NDEBUG
@@ -475,17 +554,17 @@ public:
 #endif
                     return;
             }
-            if(doBreak) {
+            if (doBreak) {
                 doBreak = false;
                 return;
             }
             if (previousProgramCounter == programCounter) {
                 std::cout << "Trap found!" << std::endl;
-                if(programCounter!=0x37ce
-                && programCounter!=0x35c9) {
+                if (programCounter != 0x37ce
+                    && programCounter != 0x35c9) {
                     return;
                 } else {
-                    programCounter=programCounter+2;
+                    programCounter = programCounter + 2;
                 }
             }
 #ifndef NDEBUG
